d8b162dc938754e52b82140d84c89be4750f7304
Ben <bjdidla1@sheffield.ac.uk>
2025-04-26 15:14:55 +0100

    Updated task_executor.py

--- gemini robotics main/robot_controller/task_executor.py
+++ gemini robotics main/robot_controller/task_executor.py
@@ -1,1 +1,41 @@
+# robot_controller/task_executor.py
+
+from robot_controller.movement import Movement
+
+class TaskExecutor:
+    """
+    Executes parsed tasks using the Movement module.
+    """
+
+    def __init__(self):
+        self.movement = Movement()
+
+    def execute(self, command: dict):
+        """
+        Execute a given command dictionary.
+        
+        Example command:
+        {
+            "action": "move_forward",
+            "distance": 2
+        }
+        """
+        action = command.get("action")
+
+        if action == "move_forward":
+            distance = command.get("distance", 1)
+            self.movement.move_forward(distance)
+        elif action == "move_backward":
+            distance = command.get("distance", 1)
+            self.movement.move_backward(distance)
+        elif action == "turn_left":
+            angle = command.get("angle", 90)
+            self.movement.turn_left(angle)
+        elif action == "turn_right":
+            angle = command.get("angle", 90)
+            self.movement.turn_right(angle)
+        elif action == "stop":
+            self.movement.stop()
+        else:
+            print(f"[TaskExecutor] Unknown action: {action}")
 
